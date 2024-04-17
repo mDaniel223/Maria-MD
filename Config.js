0449784a2d2faf8522f1cedeb0ba09522ce31a68
mDaniel223 <162805307+mDaniel223@users.noreply.github.com>
2024-04-17 17:39:18 +0800

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,24 +2,22 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
-global.ytname = "YT: AyushBotz.inc"
-global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.ownernumber = ['60103239045']
+global.ownername = "MR.COLUGO"//owner name
+global.location = "MALAYSIA"
 
-global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
+global.botname = 'COLUGO' //name of the bot
 
 //sticker details
-global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
-global.packname = 'Sticker By'
-global.author = 'Maria Bot'
+global.stickername = 'COLUGO'
+global.packname = 'Sticker By MR.COLUGO aka 41EX'
+global.author = 'MDBOT'
 //console view/theme
-global.themeemoji = '🧩'
-global.wm = "Ayush botz inc."
+global.themeemoji = '🐸'
+global.wm = "mDaniel223"
 
 //theme link
-global.link = 'https://whatsapp.com/channel/0029VaImo5ZG3R3qjKOdyr1I'
+global.link = 'https://chat.whatsapp.com/GxbhVc7Mwv2IVRcZFGG8KH'
 
 //custom prefix
 global.prefa = ['.']
